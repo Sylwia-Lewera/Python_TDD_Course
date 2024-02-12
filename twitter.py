716765c35ee2dad879fa8f1c18ab78db0b073d30
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-02-12 20:56:42 +0100

    tests parametrized with decorator

--- twitter.py
+++ twitter.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Twitter(object):
     version = '1.0'
     def __init__(self):
@@ -7,6 +10,9 @@ class Twitter(object):
         if len(message) > 160:
             raise Exception("Message too long.")
         self.tweets.append(message)
+
+    def find_hashtags(self, message):
+        return [m.lower() for m in re.findall("#(\w+)", message)]
 
 twitter = Twitter()
 print(twitter.version, twitter.tweets)
--- twitter_pytest_test.py
+++ twitter_pytest_test.py
@@ -8,13 +8,28 @@ def test_twitter_initialization():
     twitter = Twitter()
     assert twitter
 
+
 def test_tweet_single_message():
     twitter = Twitter()
     twitter.tweet('Test message')
     assert twitter.tweets == ['Test message']
 
+
 def test_tweet_long_message():
     twitter = Twitter()
-    with pytest.raises(Exception): #assertion for exception raised
+    with pytest.raises(Exception):  # assertion for exception raised
         twitter.tweet('test' * 41)
     assert twitter.tweets == []
+
+#Python decorator
+@pytest.mark.parametrize("message, expected", (
+        ("Test #first message", ["first"]),
+        ("#first Test message", ["first"]),
+        ("#FIRST Test message", ["first"]),
+        ("Test message #first", ["first"]),
+        ("Test message #first #second", ["first", "second"])
+))
+def test_tweet_with_hashtag(message, expected):
+    twitter = Twitter()
+    assert twitter.find_hashtags(message) == expected
+
