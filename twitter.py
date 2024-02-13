f27a77273bbe6b97a4b24be3dd47177410976a35
Sylwia Lewera <lewera.sylwia@gmail.com>
2024-02-13 21:00:25 +0100

    fixtures with  parametrization, backend with tempdir

--- twitter.py
+++ twitter.py
@@ -1,20 +1,34 @@
+import os
 import re
 
 
 class Twitter(object):
     version = '1.0'
-    def __init__(self):
-        self.tweets = []
+    def __init__(self, backend=None):
+        self.backend= backend
+        self._tweets = []
+    @property # getter for private class attribute
+    def tweets(self):
+        if self.backend and not self._tweets:
+                self._tweets = [line.rstrip('\n') for line in self.backend.readlines()]
+        return self._tweets
+
+    def delete(self):
+        if self.backend:
+                os.remove(self.backend)
 
     def tweet(self, message):
         if len(message) > 160:
             raise Exception("Message too long.")
         self.tweets.append(message)
+        if self.backend:
+                self.backend.write("\n".join(self.tweets))
 
     def find_hashtags(self, message):
         return [m.lower() for m in re.findall("#(\w+)", message)]
 
-twitter = Twitter()
-print(twitter.version, twitter.tweets)
-twitter.tweet('This is a test message')
-print(twitter.tweets)
+if __name__ == '__main__':
+    twitter = Twitter()
+    print(twitter.version, twitter.tweets)
+    twitter.tweet('This is a test message')
+    print(twitter.tweets)
--- twitter_pytest_test.py
+++ twitter_pytest_test.py
@@ -3,25 +3,35 @@ import pytest
 
 from twitter import Twitter
 
+@pytest.fixture
+def backend(tmpdir):
+    temp_file=tmpdir.join('test.txt')
+    temp_file.write('')
+    return temp_file
+@pytest.fixture(params=['list', 'backend'], name='twitter')  # fixture scope is global, for all tests (default scope = function)
+def fixture_twitter(backend, request):
+    if request.param == 'list':
+        twitter = Twitter()
+    elif request.param == 'backend':
+        twitter = Twitter(backend=backend)
+    return twitter
 
-def test_twitter_initialization():
-    twitter = Twitter()
+def test_twitter_initialization(twitter):
     assert twitter
 
 
-def test_tweet_single_message():
-    twitter = Twitter()
+def test_tweet_single_message(twitter):
     twitter.tweet('Test message')
     assert twitter.tweets == ['Test message']
 
 
-def test_tweet_long_message():
-    twitter = Twitter()
+def test_tweet_long_message(twitter):
     with pytest.raises(Exception):  # assertion for exception raised
         twitter.tweet('test' * 41)
     assert twitter.tweets == []
 
-#Python decorator
+
+# Python decorator
 @pytest.mark.parametrize("message, expected", (
         ("Test #first message", ["first"]),
         ("#first Test message", ["first"]),
@@ -29,7 +39,13 @@ def test_tweet_long_message():
         ("Test message #first", ["first"]),
         ("Test message #first #second", ["first", "second"])
 ))
-def test_tweet_with_hashtag(message, expected):
-    twitter = Twitter()
+def test_tweet_with_hashtag(twitter, message, expected):
     assert twitter.find_hashtags(message) == expected
 
+def test_initialize_two_twitter_classes(backend):
+    twitter1 = Twitter(backend=backend)
+    twitter2 = Twitter(backend=backend)
+
+    twitter1.tweet('Test 1')
+    twitter1.tweet('Test 2')
+    assert twitter2.tweets == ['Test 1', 'Test 2']
